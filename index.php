73b69695dc8cb0d9c6f236f65a916d54370fb93e
Mehul <mehultandale@gmail.com>
2023-01-17 07:16:48 +0530

    > Adding associative array eg

--- index.php
+++ index.php
@@ -6,7 +6,11 @@
 	// Arrays
 	$programming_languages = array("PHP", "C++", "Java");
 	echo "\n";
-	echo $programming_languages[1];
+	echo "Programming language: " . $programming_languages[1] . "\n";
+
+	// Associative arrays
+	$programming_language_versions = array("PHP" => 8, "C++" => 20, "Java" => 19);
+	echo "Version: " . $programming_language_versions[$programming_languages[1]];
 
 	// Formatting output tail
 	echo "\n\n\n";
