4861783f85e1e79343b7758426b8b88fde84b15f
Mehul <mehultandale@gmail.com>
2023-01-17 07:21:04 +0530

    > Modularising [Refactor]

---
+++ array_implementation.php
@@ -0,0 +1,12 @@
+<?php
+
+	// Arrays
+	$programming_languages = array("PHP", "C++", "Java");
+	echo "\n";
+	echo "Programming language: " . $programming_languages[1] . "\n";
+
+	// Associative arrays
+	$programming_language_versions = array("PHP" => 8, "C++" => 20, "Java" => 19);
+	echo "Version: " . $programming_language_versions[$programming_languages[1]];
+
+?>
--- index.php
+++ index.php
@@ -4,13 +4,7 @@
 	echo "\n\n". "PHP Concepts Implementation" . "\n";
 
 	// Arrays
-	$programming_languages = array("PHP", "C++", "Java");
-	echo "\n";
-	echo "Programming language: " . $programming_languages[1] . "\n";
-
-	// Associative arrays
-	$programming_language_versions = array("PHP" => 8, "C++" => 20, "Java" => 19);
-	echo "Version: " . $programming_language_versions[$programming_languages[1]];
+	include "array_implementation.php";
 
 	// Formatting output tail
 	echo "\n\n\n";
